24e05d1dfa61ac295257ae643c01d206174fe13d
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-30 06:28:51 -0400

    began routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,30 @@ import SearchBar from './SearchBar'
 import Nav from './Nav'
 import Gallery from './Gallery'
 import apiKey from '../config'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends React.Component {
+
+  state = {
+    photos = [],
+    elephants =[],
+    lions = [],
+    leopards = []
+  }
+
   render() {
     return (
       <div id="root">
-        <div className="container">
-          <SearchBar />
-          <Nav />
-          <Gallery />
+        <BrowserRouter>
+          <div className="container">
+            <SearchBar />
+            <Nav />
+            <Switch>
+              <Gallery />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
