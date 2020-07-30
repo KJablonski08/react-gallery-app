c1d22a09989d27b27f5c299b7dc131d40d1a1dd3
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-30 07:10:03 -0400

    added routes and state for link queries

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Gallery from './Gallery';
 import apiKey from '../config';
 import axios from 'axios';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import NotFound from './NotFound';
 
 class App extends React.Component {
 
@@ -16,13 +17,23 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    
+    this.performSearch('elephants')
+    this.performSearch('lions')
+    this.performSearch('leopards')
   }
 
   performSearch = (query) => {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=16&format=json&nojsoncallback=1`)
       .then(response => {
-        this.setState( { photos: response.data.photos.photo } )
+        if (query === 'elephants') {
+          this.setState( { elephants: response.data.photos.photo } )
+        } else if (query === 'lions') {
+          this.setState( { lions: response.data.photos.photo } )
+        } else if (query === 'leopards') {
+          this.setState( { leopards: response.data.photos.photo } )
+        } else {
+          this.setState( { photos: response.data.photos.photo } )
+        }
       })
       .catch(error => {
         console.log('Error fetching data from Flickr API', error);
@@ -37,7 +48,11 @@ class App extends React.Component {
             <SearchBar onSearch={this.performSearch} />
             <Nav />
             <Switch>
-              <Gallery data={this.state.photos} />
+              <Route exact path='/' render={() => <Gallery data={this.state.photos} /> } />
+              <Route path='/elephant' render={() => <Gallery data={this.state.elephants} /> } />
+              <Route path='/lion' render={() => <Gallery data={this.state.lions} /> } />
+              <Route path='/leopard' render={() => <Gallery data={this.state.leopards} /> } />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </BrowserRouter>
