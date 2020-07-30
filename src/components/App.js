48d43410f86a439d1ad261ce45558a8e451763da
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-29 21:15:26 -0400

    apiKey added to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar'
 import Nav from './Nav'
 import Gallery from './Gallery'
+import apiKey from '../config'
 
 class App extends React.Component {
   render() {
