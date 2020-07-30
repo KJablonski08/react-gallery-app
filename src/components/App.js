892529e325a70377569467631fe92b05bd20d573
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-30 06:36:57 -0400

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import SearchBar from './SearchBar'
-import Nav from './Nav'
-import Gallery from './Gallery'
-import apiKey from '../config'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import SearchBar from './SearchBar';
+import Nav from './Nav';
+import Gallery from './Gallery';
+import apiKey from '../config';
+import axios from 'axios';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends React.Component {
 
@@ -12,6 +13,17 @@ class App extends React.Component {
     elephants: [],
     lions: [],
     leopards: []
+  }
+
+  componentDidMount() {
+    let query = 'dogs'
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=20&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState( { photos: response.data.photos.photo } )
+      })
+      .catch(error => {
+        console.log('Error fetching data from Flickr API', error);
+      });
   }
 
   render() {
