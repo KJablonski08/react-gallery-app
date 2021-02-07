b3af9fc176391466b54fa05f1a156eb4ad883750
kjablonski08@gmail.com <kjablonski08@gmail.com>
2021-02-07 16:34:40 -0500

    update env variable name to work

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
 	performSearch = (query) => {
 		axios
 			.get(
-				`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.API_KEY}&tags=${query}&per_page=16&format=json&nojsoncallback=1`
+				`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_KEY}&tags=${query}&per_page=16&format=json&nojsoncallback=1`
 			)
 			.then((response) => {
 				if (query === 'elephants') {
